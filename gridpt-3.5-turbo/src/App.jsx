eb32f68e32578a150e2bd17fe57669036428a77e
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2023-09-07 19:07:58 +0200

    Updated App

--- gridpt-3.5-turbo/src/App.jsx
+++ gridpt-3.5-turbo/src/App.jsx
@@ -1,15 +1,59 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import { BrowserSerial } from "browser-serial";
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = " ";
-// "Explain things like you would to a 10 year old learning how to code."
-const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
+
+const systemMessage = { 
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
 }
 
 function App() {
+  const [connection, setConnection] = useState(false);
+  const serial = new BrowserSerial();
+
+  const turnOn = () => {
+    setConnection(true);
+  };
+
+  const turnOff = () => {
+    setConnection(false);
+  };
+
+  const sendCommand = (msg) => {
+    if (connection) {
+      serial.write(msg)
+        .then(() => {
+          console.log(`Sent command to serial port: `, msg);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
+
+  useEffect(() => {
+    if (connection) {
+      serial.connect()
+        .then(() => {
+          console.log('Connected to serial port');
+          serial.write("0000000000000000000000000001100000011000000000000000000000000000")
+        })
+        .catch((error) => {
+          console.error('Error connecting to serial port:', error);
+        });
+    } else {
+      serial.disconnect()
+        .then(() => {
+          console.log('Disconnected from serial port');
+        })
+        .catch((error) => {
+          console.error('Error disconnecting from serial port:', error);
+        });
+    }
+  }, [connection]); 
+
   const [messages, setMessages] = useState([
     {
       message: "Hello, I'm ChatGPT! Ask me anything!",
@@ -17,12 +61,12 @@ function App() {
       sender: "ChatGPT"
     }
   ]);
-  const [isTyping, setIsTyping] = useState(false);
 
   const handleSend = async (message) => {
+    sendCommand("1000000000000000000000000001100000011000000000000000000000000001");
 
     // draw an "up arrow" in a 8x8 binary grid. use 1 for on and 0 for off. output only a code snippet in plain text with no commas, so nothing else except the numbers
-    message = "Draw a " + message + " in an 8x8 binary grid. use 1 for on and 0 for off. output only a code snippet in plain text with no commas and nothing else except the numbers"; 
+    message = "Draw a " + message + " in an 8x8 binary grid. use 1 for on and 0 for off. output only a code snippet in plain text with no commas and nothing else except the numbers. the numbers should be surrounded by ``` on both sides"; 
 
     const newMessage = {
       message,
@@ -31,18 +75,11 @@ function App() {
     };
 
     const newMessages = [...messages, newMessage];
-    
     setMessages(newMessages);
-
-    setIsTyping(true);
     await processMessageToChatGPT(newMessages);
   };
 
   async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
-    // Format messages for chatGPT API
-    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-    // So we need to reformat
-
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
       if (messageObject.sender === "ChatGPT") {
@@ -53,15 +90,11 @@ function App() {
       return { role: role, content: messageObject.message}
     });
 
-
-    // Get the request body set up with the model we plan to use
-    // and the messages which we formatted above. We add a system message in the front to'
-    // determine how we want chatGPT to act. 
     const apiRequestBody = {
       "model": "gpt-3.5-turbo",
       "messages": [
-        systemMessage,  // The system message DEFINES the logic of our chatGPT
-        ...apiMessages // The messages from our chat with ChatGPT
+        systemMessage, 
+        ...apiMessages
       ]
     }
 
@@ -69,48 +102,56 @@ function App() {
     {
       method: "POST",
       headers: {
-        "Authorization": "Bearer " + "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",
+        "Authorization": "Bearer " + "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",
         "Content-Type": "application/json"
       },
       body: JSON.stringify(apiRequestBody)
     }).then((data) => {
       return data.json();
     }).then((data) => {
-      console.log(data);
+      // console.log(data);
       setMessages([...chatMessages, {
         message: data.choices[0].message.content,
         sender: "ChatGPT"
       }]);
-      setIsTyping(false);
+      // console.log(data.choices[0].message.content.replace(/(\r\n|\n|\r)/gm,"").match(/```([^`]+)```/));
+      const match = data.choices[0].message.content.replace(/(\r\n|\n|\r)/gm,"").match(/```([^`]+)```/);
+      if (match && match[1]) {
+        console.log("sending...", match[1]);
+        // console.log(match[1].replace(/(\r\n|\n|\r)/gm,""));
+        sendCommand(match[1]);
+      }
     });
   }
-
 
   return (
     <div className="App">
       <div className='input-box'> 
+        <button className='connect' onClick={turnOn}>connect</button>
+        <button className='disconnect' onClick={turnOff}>disconnect</button>
+        <button onClick={() => sendCommand("0001100000111100011111100001100000011000000110000000000000000000")}>Test Command</button>
+        <button onClick={() => sendCommand("0001100000111100011111100001100000011000000110000000000000000011")}>Test Command 2</button>
+        <br/>
         Draw a
         <MessageInput placeholder="circle" onSend={handleSend} />  
       </div>
-        <MainContainer>
+        {/* <MainContainer>
           <ChatContainer>       
             <MessageList 
               scrollBehavior="smooth" 
-              typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
+              typingIndicator={null}
             >
               {messages.map((message, i) => {
-                const match = message.message.match(/```plain text([^`]+)```/);
-
-                if (match && match[1]) {
-                  console.log(match[1]);
-                }
+                // const match = message.message.match(/```([^`]+)```/);
+                // if (match && match[1]) {
+                  // console.log(match[1].replace(/(\r\n|\n|\r)/gm,""));
+                  // sendCommand(match[1].replace(/(\r\n|\n|\r)/gm,"") + "\n");
+                // }
                 return <Message key={i} model={message} />
               })}
             </MessageList>
-            
-                  
           </ChatContainer>
-        </MainContainer>
+        </MainContainer> */}
     </div>
   )
 }
