74cf09645d0993005d3bda8940782b3e239b7564
Kay van den Aker <34678030+kayvandenaker@users.noreply.github.com>
2023-09-08 09:08:44 +0200

    update main app

--- gridpt-3.5-turbo/src/App.jsx
+++ gridpt-3.5-turbo/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { BrowserSerial } from "browser-serial";
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import { MessageInput } from '@chatscope/chat-ui-kit-react';
 
 
 const systemMessage = { 
@@ -13,19 +13,14 @@ function App() {
   const [connection, setConnection] = useState(false);
   const serial = new BrowserSerial();
 
-  const turnOn = () => {
-    setConnection(true);
-  };
-
-  const turnOff = () => {
-    setConnection(false);
-  };
+  const turnOn = () => { setConnection(true); };
+  const turnOff = () => { setConnection(false); };
 
   const sendCommand = (msg) => {
     if (connection) {
       serial.write(msg)
         .then(() => {
-          console.log(`Sent command to serial port: `, msg);
+          console.log(`Sent to serial: `, msg);
         })
         .catch((error) => {
           console.error(error);
@@ -62,32 +57,15 @@ function App() {
     }
   }, [connection]); 
 
-  const [messages, setMessages] = useState([
-    {
-      message: "Hello, I'm ChatGPT! Ask me anything!",
-      sentTime: "just now",
-      sender: "ChatGPT"
-    }
-  ]);
-
   const handleSend = async (message) => {
-    sendCommand("1000000000000000000000000001100000011000000000000000000000000001");
+    sendCommand("00111100 01111110 01100110 00001100 00011000 00000000 00011000 00011000");
+    console.log("Fetching data for: ", message);
 
-    // draw an "up arrow" in a 8x8 binary grid. use 1 for on and 0 for off. output only a code snippet in plain text with no commas, so nothing else except the numbers
     message = "Draw a " + message + " in an 8x8 binary grid. use 1 for on and 0 for off. output only a code snippet in plain text with no commas and nothing else except the numbers. the numbers should be surrounded by ``` on both sides"; 
-
-    const newMessage = {
-      message,
-      direction: 'outgoing',
-      sender: "user"
-    };
-
-    const newMessages = [...messages, newMessage];
-    // setMessages(newMessages);
-    await processMessageToChatGPT(newMessages);
+    await processMessageToChatGPT([{ message, direction: 'outgoing', sender: "user" }]);
   };
 
-  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
+  async function processMessageToChatGPT(chatMessages) { 
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
       if (messageObject.sender === "ChatGPT") {
@@ -98,19 +76,13 @@ function App() {
       return { role: role, content: messageObject.message}
     });
 
-    const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        systemMessage, 
-        ...apiMessages
-      ]
-    }
+    const apiRequestBody = { "model": "gpt-4", "messages": [ systemMessage, ...apiMessages ] }
 
     await fetch("https://api.openai.com/v1/chat/completions", 
     {
       method: "POST",
       headers: {
-        "Authorization": "Bearer " + "api key",
+        "Authorization": "Bearer " + "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",
         "Content-Type": "application/json"
       },
       body: JSON.stringify(apiRequestBody)
@@ -118,15 +90,8 @@ function App() {
       return data.json();
     }).then((data) => {
       // console.log(data);
-      // setMessages([...chatMessages, {
-      //   message: data.choices[0].message.content,
-      //   sender: "ChatGPT"
-      // }]);
-      // console.log(data.choices[0].message.content.replace(/(\r\n|\n|\r)/gm,"").match(/```([^`]+)```/));
       const match = data.choices[0].message.content.replace(/(\r\n|\n|\r)/gm,"").match(/```([^`]+)```/);
       if (match && match[1]) {
-        console.log("sending...", match[1]);
-        // console.log(match[1].replace(/(\r\n|\n|\r)/gm,""));
         sendCommand(match[1]);
       }
     });
@@ -134,33 +99,16 @@ function App() {
 
   return (
     <div className="App">
+        <div className='connection'>{connection ? <button className='disconnect' onClick={turnOff}>disconnect</button> : <button className='connect' onClick={turnOn}>connect</button>}</div>
       <div className='input-box'> 
-        <button className='connect' onClick={turnOn}>connect</button>
-        <button className='disconnect' onClick={turnOff}>disconnect</button>
-        <button onClick={() => sendCommand("0001100000111100011111100001100000011000000110000000000000000000")}>Test Command</button>
-        <button onClick={() => sendCommand("0001100000111100011111100001100000011000000110000000000000000011")}>Test Command 2</button>
-        <button onClick={() => randomCommand()}>Test Command 2</button>
-        <br/>
         Draw a
         <MessageInput placeholder="circle" onSend={handleSend} />  
+
+        <br/>
+        <button onClick={() => sendCommand("0001100000111100011111100001100000011000000110000001100000011000")}>↑</button>
+        <button onClick={() => randomCommand()}>random</button>
+        <button onClick={() => handleSend("rectangle")}>rect</button>
       </div>
-        {/* <MainContainer>
-          <ChatContainer>       
-            <MessageList 
-              scrollBehavior="smooth" 
-              typingIndicator={null}
-            >
-              {messages.map((message, i) => {
-                // const match = message.message.match(/```([^`]+)```/);
-                // if (match && match[1]) {
-                  // console.log(match[1].replace(/(\r\n|\n|\r)/gm,""));
-                  // sendCommand(match[1].replace(/(\r\n|\n|\r)/gm,"") + "\n");
-                // }
-                return <Message key={i} model={message} />
-              })}
-            </MessageList>
-          </ChatContainer>
-        </MainContainer> */}
     </div>
   )
 }
